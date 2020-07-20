abe9f75ba691c0db9f951d494817fb6eafb0b65d
Fonz2424 <65732402+Fonz2424@users.noreply.github.com>
2020-07-20 15:15:02 -0400

    Add files via upload

---
+++ javascript-lesson-3-intro-to-functions/README.markdown
@@ -0,0 +1,5 @@
+# JavaScript-Lesson-3-Intro-To-Functions
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/Fonz2424/pen/ZEQEqdO](https://codepen.io/Fonz2424/pen/ZEQEqdO).
+
+
---
+++ javascript-lesson-3-intro-to-functions/src/script.js
@@ -0,0 +1,23 @@
+ /*
+ 
+exercise #1: variables and number data type
+
+var num1 = 7;
+var num2 = 3;
+var sum = num1 + num2;
+console.log(sum);
+
+exercise #2 :function example #1
+
+function sumNumbers(num1, num2){
+  var sum = num1 + num2; 
+  return sum;
+ }  
+ 
+var output = sumNumbers(76,49);
+console.log(output);
+*/
+//exercise #3 : JavaScript arrow function
+
+const sumNumbers = (num1,num2) => {return num1 + num2};
+document.getElementById("addingNumbers").innerHTML =sumNumbers(2,5);
